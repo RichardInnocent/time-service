898ebd190af58a36bae7d877245e04e72443f96a
RichardInnocent <richjinnocent@gmail.com>
2022-06-20 19:07:00 +0100

    fix: Initial commit

---
+++ src/test/java/org/richardinnocent/timeservice/ApplicationTest.java
@@ -0,0 +1,12 @@
+package org.richardinnocent.timeservice;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+  @Test
+  public void constructor_always_runs() {
+    new Application();
+  }
+
+}
