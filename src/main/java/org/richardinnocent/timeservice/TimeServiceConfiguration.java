2ae5b768b783e9a145058cc0109b9274beb16e7c
RichardInnocent <richjinnocent@gmail.com>
2022-06-26 17:55:43 +0100

    fix(callbacks): Fixed formatting of times

--- src/main/java/org/richardinnocent/timeservice/TimeServiceConfiguration.java
+++ src/main/java/org/richardinnocent/timeservice/TimeServiceConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
@@ -25,6 +26,7 @@ public class TimeServiceConfiguration {
   public ObjectMapper objectMapper() {
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.registerModule(new JavaTimeModule());
+    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
     return objectMapper;
   }
 
